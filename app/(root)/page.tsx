30872aebec2714c4ed3e40a59d4f5f6c66c56105
ricardomadeira93 <ricardomadeira1993@gmail.com>
2023-08-07 14:45:12 +0100

    middleware

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+const SetupPage = () => {
+  return (
+    <div className=''>
+      <Button></Button>
+    </div>
+  );
+};
+
+export default SetupPage;
